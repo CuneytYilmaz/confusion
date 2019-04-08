fc327f7292ad8191f4d738c5da68236dc52ef5cf
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-04-08 23:38:39 +0300

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// test
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
